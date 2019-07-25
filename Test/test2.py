cf95e8044f0dcc69c95d07556dfb2e62827286b6
dshahid380 <dshahid3805@gmail.com>
2019-07-25 20:51:43 +0530

    Problem Added

--- Test/test2.py
+++ Test/test2.py
@@ -1,9 +1,9 @@
 import math
-from sys import stdin,stdout, maxsize
+from sys import stdin,stdout
 from bisect import bisect_left, bisect_right
 
-def isprime(n) :  
-    if(n<=1) : 
+def isprime(n):  
+    if(n<=1): 
        return False
     if(n<=3) : 
        return True
@@ -15,7 +15,6 @@ def isprime(n) :
            return False
         i=i+6
     return True
-
 def freq(l): #return freq disctionary   
     f = {} 
     for i in l: 
@@ -25,46 +24,40 @@ def freq(l): #return freq disctionary
           f[i] = 1
     return f
 
+MAX = pow(10, 5)
 inp=lambda : int(stdin.readline())
 sip=lambda : stdin.readline()
 mulip =lambda : map(int, stdin.readline().split())
 lst=lambda : list(map(int,stdin.readline().split()))
 slst=lambda: list(sip())
 #-------------------------------------------------------
+for _ in range(inp()):
+    n = inp()
+    maxx, maxy, minx, miny = MAX, MAX, -MAX, -MAX
+    for _ in range(n):
+        x, y, f1, f2, f3, f4 = mulip()
+        if f2==0:
+            maxy = min(maxy, y)
+        if f4==0:
+            miny = max(miny, y)
+        if f3==0:
+            maxx = min(maxx, x)
+        if f1==0:
+            minx = max(minx, x)
+    if minx <= maxx and miny <= maxy:
+        print(1,minx,miny)
+    else:
+        print(0)
 
-def find_max_cost(a,size, k, m): 
-  
-    max_so_far = -maxsize - 1
-    max_ending_here = 0
-    start = 0
-    end = 0
-    s = 0
-  
-    for i in range(0,size): 
-  
-        max_ending_here += a[i] 
-  
-        if max_so_far < max_ending_here: 
-            max_so_far = max_ending_here 
-            start = s 
-            end = i 
-  
-        if max_ending_here < 0: 
-            max_ending_here = 0
-            s = i+1
-  
-    ans = max_so_far - k*math.ceil((end-start+1)/m) 
-    return ans
+
     
 
-n, m, k = mulip()
-A = lst()
-print(find_max_cost(A, n, k, m))
 
 
 
 
 
 
+  
 
   
--- practice/test.c
+++ practice/test.c
@@ -1,45 +1,10 @@
 #include <stdio.h> 
+#include<conio.h>
+#include<string.h>
   
-int a[100][100];  
-void spiralPrint(int m, int n, int a[100][100]) 
-{ 
-    int i, k = 0, l = 0; 
-  
-    while (k < m && l < n) { 
-        for (i = l; i < n; ++i) { 
-            printf("%d ", a[k][i]); 
-        } 
-        k++; 
-  
-        for (i = k; i < m; ++i) { 
-            printf("%d ", a[i][n - 1]); 
-        } 
-        n--; 
-  
-        if (k < m) { 
-            for (i = n - 1; i >= l; --i) { 
-                printf("%d ", a[m - 1][i]); 
-            } 
-            m--; 
-        } 
-        if (l < n) { 
-            for (i = m - 1; i >= k; --i) { 
-                printf("%d ", a[i][l]); 
-            } 
-            l++; 
-        } 
-    } 
-} 
-
 int main() 
-{ 
-    int m, n, i, j;
-    scanf("%d %d", &n, &m);
-    for(i=0; i<n; i++){
-        for(j=0; j<m; j++){
-            scanf("%d",&a[i][j]);
-      }
-    }
-    spiralPrint(n,m,a); 
+{   
+    int a = 1;
+    printf("%d %d %d", a, ++a, a++);
     return 0; 
-} 
+}
