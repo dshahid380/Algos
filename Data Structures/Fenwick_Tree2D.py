f195cb82e9c63a31446fca6cceefab78342e40d8
dshahid380 <dshahid3805@gmail.com>
2019-07-30 12:13:02 +0530

    2D Fenwick Tree added

---
+++ Data Structures/Fenwick_Tree2D.py
@@ -0,0 +1,45 @@
+def update(x, y, val):
+	i = x
+	while(i < len(A)):
+		j = y
+		while(j < len(A[0])):
+			tree[i][j] += val
+			j = j | (j+1)
+		i = i | (i+1) 
+
+def sum_upto(x, y):
+	res = 0 
+	i = x 
+	while(i >= 0):
+		j = y
+		while(j >= 0):
+			res += tree[i][j]
+			j = (j & (j+1)) - 1
+		i = (i & (i+1)) - 1
+	return res 
+
+def query(x1, y1, x2, y2):
+	ans = sum_upto(x2, y2) + sum_upto(x1-1, y1-1) - sum_upto(x2, y1-1) - sum_upto(x1-1, y2)
+	return ans 
+
+def build():
+	for i in range(len(A)):
+		for j in range(len(A[0])):
+			update(i, j, A[i][j])
+
+N, M = map(int, input().split())
+A = [[int(x) for x in input().split()] for i in range(N)]
+tree = [[0]*M for i in range(N)]
+build()
+
+for i in tree:
+	print(*i)
+
+print(query(2,2,4,4))
+update(4,4,2)
+print(query(2,2,4,4))
+
+
+
+
+
