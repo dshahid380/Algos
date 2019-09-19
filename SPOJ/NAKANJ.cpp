4c89cc83c0a2bd1ceb2191f9c39434ac75d828da
dshahid380 <dshahid3805@gmail.com>
2019-09-19 11:43:50 +0530

    SPOJ solutions, Graphs & CodeChef solutions added

--- SPOJ/NAKANJ.cpp
+++ SPOJ/NAKANJ.cpp
@@ -5,54 +5,54 @@ using namespace std;
 #define pii <ll, ll>
 
 int main() {
-	 int T;
-	 cin>>T;
-	 string ch[] = {"a","b","c","d","e","f","g","h"};
-	 unordered_map<string,vector<string>> adj;
-	 unordered_map<string,bool> visited;
-	 unordered_map<string,int> distance;
-	 for(int i=0; i<8;i++){
-	 	  for(int j=0; j<8; j++){
-	 	  	  visited[ch[i]+to_string(j+1)]=false;
-	 	  	  distance[ch[i]+to_string(j+1)]=0;
-	 	     	for(int k=0; k<8; k++){
-	 	     	   for(int l=0; l<8; l++){
-	 	     	   	   if(((i-k)*(i-k)+(j-l)*(j-l))==5){
-	 	     	   	   	  adj[ch[i]+to_string(j+1)].push_back(ch[k]+to_string(l+1));
-								}
-						 }	
-					}
-		  }	     
-	 }
-	 
-	 while(T--) {
-	 	  unordered_map<string, bool> used;
-	 	  used = visited;
-	 	  queue<string> q;
-	 	  string s, d;
-	 	  cin>>s>>d;
-	 	  q.push(s);
-	 	  used[s]=true;
-	 	  unordered_map<string,int> dis;
-	 	  dis[s]=0;
-	 	  int f=0;
-	 	  while(!q.empty()) {
-	 	  	 string v = q.front();
-	 	  	 //cout<<v<<endl;
-				 q.pop();
-				 for(auto u : adj[v]) {
-				 	  if(!used[u]) {
-				 	  	  used[u]=true;
-				 	  	  q.push(u);
-				 	  	  dis[u] = dis[v]+1;
-				 	  	  //cout<<dis[u]<<endl;
-				 	  	      			 	  	  
-						}
-				 }
-					  
-			}
-			cout<<dis[d]<<endl;
-	 }
-	 return 0;
-	 
+     int T;
+     cin>>T;
+     string ch[] = {"a","b","c","d","e","f","g","h"};
+     unordered_map<string,vector<string>> adj;
+     unordered_map<string,bool> visited;
+     unordered_map<string,int> distance;
+     for(int i=0; i<8;i++){
+          for(int j=0; j<8; j++){
+              visited[ch[i]+to_string(j+1)]=false;
+              distance[ch[i]+to_string(j+1)]=0;
+                for(int k=0; k<8; k++){
+                   for(int l=0; l<8; l++){
+                       if(((i-k)*(i-k)+(j-l)*(j-l))==5){
+                          adj[ch[i]+to_string(j+1)].push_back(ch[k]+to_string(l+1));
+                                }
+                         }  
+                    }
+          }      
+     }
+     
+     while(T--) {
+          unordered_map<string, bool> used;
+          used = visited;
+          queue<string> q;
+          string s, d;
+          cin>>s>>d;
+          q.push(s);
+          used[s]=true;
+          unordered_map<string,int> dis;
+          dis[s]=0;
+          int f=0;
+          while(!q.empty()) {
+             string v = q.front();
+             //cout<<v<<endl;
+                 q.pop();
+                 for(auto u : adj[v]) {
+                      if(!used[u]) {
+                          used[u]=true;
+                          q.push(u);
+                          dis[u] = dis[v]+1;
+                          //cout<<dis[u]<<endl;
+                                                  
+                        }
+                 }
+                      
+            }
+            cout<<dis[d]<<endl;
+     }
+     return 0;
+     
 }
